3fcf6bcde49b225500c3fcdb336388d618fb272a
Daisy Lin <daisy@Daisy>
2023-10-26 10:31:59 +1100

    Refactored code, and fixed loop bug

--- src/search.py
+++ src/search.py
@@ -1,55 +1,46 @@
-from tabulate import tabulate 
+from tabulate import tabulate
 from ast import literal_eval
+
 
 def get_type(input_data):
     try:
         return type(literal_eval(input_data))
-    except(ValueError, SyntaxError):
+    except (ValueError, SyntaxError):
         return str
 
-
-    
 def search_method(data, fullheaders):
-    running = True
     
+    search_again_text = 'Do you want to search again? [Y]/[N]: '
+    running = True
+
     while running:
-        user_input = input('Enter a pokemon name or ID number to search\n\n')
 
+        user_input = input('Enter a Pokémon name or ID number to search: ')
         input_type = get_type(user_input)
-        search_pokemon(user_input, input_type, data, fullheaders)
-        
-        while True:
-            user_question = input('Do you want to search again? [Y]/[N] \n')
 
-            if user_question.upper() == "Y":
-                break
-            elif user_question.upper() == "N":
+        if search_pokemon(user_input, input_type, data, fullheaders):
+            user_question = input(search_again_text)
+            while user_question.upper() not in ["Y", "N"]:
+                user_question = input('Please enter "Y" or "N".\n')
+            if user_question.upper() == "N":
                 running = False
-                break
-            else:
-                print('Please enter "Y" or "N".\n')
+        else:
+            print("Pokemon doesn't exist.")
 
 
-
-def search_pokemon(id, type ,data, fullheaders):
+def search_pokemon(id, type, data, fullheaders):
 
     if type == str:
         col = 1
         id = id.lower()
     else:
         col = 0
-    
+
     column = [x[col].lower() for x in data]
 
     if id in column:
-        i = column.index(id)        
-     
-        print(tabulate([data[i]], headers= fullheaders, tablefmt="fancy_grid"))
+        i = column.index(id)
 
-    else:
-        print("Pokemon doesn't exist.")
-
-
-    
-    
+        print(tabulate([data[i]], headers=fullheaders, tablefmt="fancy_grid"))
+        return True
     
