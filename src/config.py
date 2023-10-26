464d907fbc2421bd8827bbdd4dc8a418614be63a
Daisy Lin <daisy@Daisy>
2023-10-26 14:35:32 +1100

    added config file to hold search history and variables, updated search function to include history

---
+++ src/config.py
@@ -0,0 +1,9 @@
+#Added error message to avoid repetitve code
+error_message = print('Please enter a valid reply.')
+#Declared variable in config module to avoid global requirement used in previous main.py
+main_run = True
+#empty list stored in config so search history can be retained when moving between funcs
+search_history = []
+
+#Installed packages:
+#pip install tabulate
--- src/search.py
+++ src/search.py
@@ -1,7 +1,9 @@
 import csv_module as cm
+import config as cg
 from tabulate import tabulate
 from ast import literal_eval
 
+history = cg.search_history
 
 def get_type(input_data):
     try:
@@ -9,25 +11,43 @@ def get_type(input_data):
     except (ValueError, SyntaxError):
         return str
 
-def search_method():
+def main_search_loop():
     
-    search_again_text = 'Do you want to search again? [Y]/[N]: '
     running = True
-
+    
     while running:
+        
+        history_switch = input("[1] Search Pokemon \n[2] View Search History \n[3] Return to Main Menu \n")
 
+        if int(history_switch) not in [1, 2, 3]:
+            cg.Error_message
+            
+        elif int(history_switch) == 1:
+            inner_search_loop()
+
+        elif int(history_switch) == 2:
+            if len(history) < 1:
+                print('No search history! Please search some pokemon first.')
+            else:
+                print(tabulate(history,headers= ["Searches:","Results - ID:", "Result - Name:"], tablefmt="fancy_grid"))
+
+        elif int(history_switch) == 3:
+            running = False
+
+def inner_search_loop():
+    while True:
         user_input = input('Enter a Pokémon name or ID number to search: ')
         input_type = get_type(user_input)
 
-        if search_pokemon(user_input, input_type):
-            user_question = input(search_again_text)
-            while user_question.upper() not in ["Y", "N"]:
-                user_question = input('Please enter "Y" or "N".\n')
-            if user_question.upper() == "N":
-                running = False
-        else:
-            print("Pokemon doesn't exist.")
+        result = search_pokemon(user_input, input_type)
+        history.append([user_input] + result)
 
+        user_question = input('Do you want to search again? [Y]/[N]: ')
+        while user_question.upper() not in ["Y", "N"]:
+            user_question = input('Please enter "Y" or "N".\n')
+
+        if user_question.upper() == "N":
+            break
 
 def search_pokemon(id, type):
 
@@ -41,7 +61,9 @@ def search_pokemon(id, type):
 
     if id in column:
         i = column.index(id)
-
-        print(tabulate([cm.data[i]], headers=cm.headers, tablefmt="fancy_grid"))
-        return True
+        print(tabulate([cm.data[i]], headers=cm.headers, tablefmt='fancy_grid'))
+        return cm.data[i][:2]
+    else:
+        print("Pokémon doesn't exist.")
+        return [None, None]
     
